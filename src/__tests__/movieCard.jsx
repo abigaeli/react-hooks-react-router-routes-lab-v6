fe1266af790b286b35fddbb520f95a90a38a63c1
abigaeli <abigaelmacharia7@gmail.com>
2025-04-28 00:07:54 +0300

    complete

---
+++ src/__tests__/movieCard.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function MovieCard({ movie }) {
+  return (
+    <div>
+      <h2>{movie.title}</h2>
+      <Link to={`/movie/${movie.id}`}>View Details</Link>
+    </div>
+  );
+}
+
+export default MovieCard;
